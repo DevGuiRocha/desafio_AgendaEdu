fb65408889e2e58867da191df1cb7c3d3b08d08b
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-05-17 18:19:28 -0300

    feat(deputados): incluído maior_despesa no json dretorno do controller

--- ceap-backend/app/controllers/deputados_controller.rb
+++ ceap-backend/app/controllers/deputados_controller.rb
@@ -3,7 +3,7 @@ class DeputadosController < ApplicationController
     deputados = Deputado.where(sg_uf: 'CE')
                         .left_joins(:despesas)
                         .select(
-                          'deputados.*, COALESCE(SUM(despesas.vlr_liquido), 0) AS total_despesas'
+                          'deputados.*, COALESCE(SUM(despesas.vlr_liquido), 0) AS total_despesas, MAX(despesas.vlr_liquido) AS maior_despesa'
                         )
                         .group("deputados.id")
 
@@ -14,6 +14,7 @@ class DeputadosController < ApplicationController
         nome_parlamentar: d.nome_parlamentar,
         sg_uf: d.sg_uf,
         sg_partido: d.sg_partido,
+        maior_despesa: d.maior_despesa.to_f,
         total_gastos: d.total_despesas.to_f
       }
     }
