67d973e4626d34c6224afc6677e6a11929fcce95
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-05-17 18:13:18 -0300

    feat(deputados): ajustada exibição em tabela e incluída foto dos deputados para impressão

--- ceap-frontend/src/pages/Deputados.jsx
+++ ceap-frontend/src/pages/Deputados.jsx
@@ -14,15 +14,37 @@ export default function Deputados() {
     return (
         <div>
             <h2>Lista de Deputados (CE)</h2>
-            <ul>
-                {deputados.map(d => (
-                    <li key={d.id}>
-                        <Link to={`/deputados/${d.id}/despesas`}>
-                        {d.nome_parlamentar} - Total gasto: R$ {d.total_gastos.toFixed(2)}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Foto Deputado</th>
+                        <th>Nome Deputado</th>
+                        <th>Partido</th>
+                        <th>Maior Despesa</th>
+                        <th>Total gasto</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {deputados.map(d => (
+                        <tr key={d.id}>
+                            <td>
+                                <img src={`http://www.camara.leg.br/internet/deputado/bandep/${d.ide_cadastro}.jpg`} 
+                                    style={{ width: 50, height: 50, objectFit: 'cover', borderRadius: '50%' }} />
+                            </td>
+                            <td>{d.nome_parlamentar}</td>
+                            <td>{d.sg_partido}</td>
+                            <td>{d.maior_despesa.toFixed(2)}</td>
+                            <td>{d.total_gastos.toFixed(2)}</td>
+                            <td>
+                                <Link to={`/deputados/${d.id}/despesas`}>
+                                    Verificar Gastos
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
