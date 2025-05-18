ba48f122c119610f7b3bf5a3098d05269668cc57
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-05-17 23:31:08 -0300

    test: adicionar testes de validações e associações para models Deputado e Despesa

--- ceap-backend/spec/models/deputado_spec.rb
+++ ceap-backend/spec/models/deputado_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Deputado, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:deputado) }
+
+  describe 'Verificação das associações do Model Deputado' do
+    it { is_expected.to have_many(:despesas).dependent(:destroy) }
+  end
+
+  describe 'Verificação das validações do Model Deputado' do
+    it { is_expected.to validate_presence_of(:ide_cadastro) }
+    it { is_expected.to validate_uniqueness_of(:ide_cadastro) }
+    it { is_expected.to validate_presence_of(:nome_parlamentar) }
+    it { is_expected.to validate_presence_of(:sg_uf) }
+    it { is_expected.to validate_presence_of(:cpf) }
+  end
 end
--- ceap-backend/spec/models/despesa_spec.rb
+++ ceap-backend/spec/models/despesa_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Despesa, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:despesa) }
+
+  describe 'Verificação das associações do Model Despesa' do
+    it { is_expected.to belong_to(:deputado) }
+  end
+
+  describe 'Verificação das validações do Model Despesa' do
+    it { is_expected.to validate_presence_of(:dat_emissao) }
+    it { is_expected.to validate_presence_of(:txt_fornecedor) }
+
+    it do
+      is_expected.to validate_numericality_of(:vlr_liquido).is_greater_than_or_equal_to(0)
+    end
+  end
 end
