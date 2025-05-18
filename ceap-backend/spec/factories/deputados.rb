869521deb0f9d53881ce322d3a3fdb6024f4aa25
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-05-17 22:59:26 -0300

    test(factories): adicionar dados padrão para Deputado e Despesa no ambiente de testes

--- ceap-backend/spec/factories/deputados.rb
+++ ceap-backend/spec/factories/deputados.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :deputado do
     ide_cadastro { 1 }
-    nome_parlamentar { "MyString" }
-    cpf { "MyString" }
-    sg_uf { "MyString" }
-    sg_partido { "MyString" }
-    nu_carteira_parlamentar { "MyString" }
-    nu_legislatura { 1 }
-    cod_legislatura { 1 }
-    nu_deputado_id { 1 }
+    nome_parlamentar { "Deputado Teste 01" }
+    cpf { "12345678901" }
+    sg_uf { "CE" }
+    sg_partido { "ABC" }
+    nu_carteira_parlamentar { "01" }
+    nu_legislatura { 2025 }
+    cod_legislatura { 58 }
+    nu_deputado_id { 1212 }
   end
 end
--- ceap-backend/spec/factories/despesas.rb
+++ ceap-backend/spec/factories/despesas.rb
@@ -1,27 +1,31 @@
 FactoryBot.define do
   factory :despesa do
-    deputado { nil }
-    dat_emissao { "2025-05-17" }
-    num_mes { 1 }
-    num_ano { 1 }
+    association :deputado
+    dat_emissao { Date.today }
+    num_mes { Date.today.month }
+    num_ano { Date.today.year }
+
     num_sub_cota { 1 }
-    txt_descricao { "MyString" }
+    txt_descricao { "Serviço de teste" }
     num_especificacao_subcota { 1 }
-    txt_descricao_especificacao { "MyString" }
-    txt_fornecedor { "MyString" }
-    txt_cnpj_cpf { "MyString" }
-    txt_numero { "MyString" }
-    ind_tipo_documento { 1 }
-    ide_documento { 1 }
-    vlr_documento { 9.99 }
-    vlr_glosa { 9.99 }
-    vlr_liquido { 9.99 }
-    num_parcela { 1 }
-    txt_passageiro { "MyString" }
-    txt_trecho { "MyString" }
-    num_lote { 1 }
-    num_ressarcimento { 1.5 }
-    dat_pagamento_restituicao { "2025-05-17 00:03:26" }
-    vlr_restituicao { 9.99 }
+    txt_descricao_especificacao { "Subcategoria de teste" }
+
+    txt_fornecedor { "Fornecedor Confiável" }
+    txt_cnpj_cpf { "123456780000199" }
+    txt_numero { "A001" }
+    ind_tipo_documento { 0 }
+    ide_documento { 999 }
+
+    vlr_documento { 100.99 }
+    vlr_glosa { 0.00 }
+    vlr_liquido { 100.99 }
+
+    num_parcela { nil }
+    txt_passageiro { nil }
+    txt_trecho { nil }
+    num_lote { nil }
+    num_ressarcimento { nil }
+    dat_pagamento_restituicao { nil }
+    vlr_restituicao { nil }
   end
 end
