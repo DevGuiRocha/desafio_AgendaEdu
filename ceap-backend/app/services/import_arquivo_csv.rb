5f6c7b195f2675397ff6a6cfa6e28fa7c9cf0dc2
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-05-26 14:08:46 -0300

    refactor: reestruturada lógica de importação do arquivo CSV para melhorar performance e legibilidade

--- ceap-backend/app/services/import_arquivo_csv.rb
+++ ceap-backend/app/services/import_arquivo_csv.rb
@@ -11,75 +11,82 @@ class ImportArquivoCsv
 
   def import!
     sucesso = 0
-    erro_valor = 0
-    erro_data = 0
+
+    deputados_cache = Deputado.where(sg_uf: 'CE')
+                              .pluck(:ide_cadastro, :id)
+                              .to_h
+
+    despesas_cache = Despesa.where(deputado_id: deputados_cache.values)
+                              .pluck(:deputado_id, :ide_documento, :id)
+                              .each_with_object({}) do |(dep_id, doc_id, desp_id), h|
+                                h[[dep_id, doc_id]] = desp_id
+                              end
 
     csv_options = { headers: true, col_sep: ";", encoding: "bom|utf-8" }
+
     CSV.foreach(@file_path, **csv_options) do |row|
-      next unless row["sgUF"] == "CE"
+      next unless row['sgUF'] == 'CE' && row["datEmissao"].present? && row["vlrLiquido"].to_d >= 0
 
-      deputado = Deputado.find_or_initialize_by(ide_cadastro: row["ideCadastro"].to_i)
+      ide = row['ideCadastro'].to_i
+      dep_id = deputados_cache[ide] || begin
+        dep_attrs = {
+          ide_cadastro: ide,
+          nome_parlamentar: row["txNomeParlamentar"],
+          cpf: row["cpf"].to_s,
+          sg_uf: row["sgUF"],
+          sg_partido: row["sgPartido"],
+          nu_carteira_parlamentar: row["nuCarteiraParlamentar"],
+          nu_legislatura: row["nuLegislatura"].to_i,
+          cod_legislatura: row["codLegislatura"].to_i,
+          nu_deputado_id: row["nuDeputadoId"].to_i
+        }
 
-      deputado.nome_parlamentar = row["txNomeParlamentar"]
-      deputado.cpf = row["cpf"].to_s
-      deputado.sg_uf = row["sgUF"]
-      deputado.sg_partido = row["sgPartido"]
-      deputado.nu_carteira_parlamentar = row["nuCarteiraParlamentar"]
-      deputado.nu_legislatura = row["nuLegislatura"].to_i
-      deputado.cod_legislatura = row["codLegislatura"].to_i
-      deputado.nu_deputado_id = row["nuDeputadoId"].to_i
-      
-      deputado.save! if deputado.changed?
-
-      despesa = Despesa.find_or_initialize_by(
-        deputado: deputado,
-        ide_documento: row["ideDocumento"].to_i
-      )
-
-      if row["datEmissao"].present?
-        despesa.dat_emissao = Date.parse(row["datEmissao"])
-      else
-        Rails.logger.warn "Ignorando despesa ideDocumento=#{row['ideDocumento']} por datEmissao inválida"
-        erro_data += 1
-        next
+        deputado = Deputado.find_or_initialize_by(ide_cadastro: ide)
+        deputado.assign_attributes(dep_attrs)
+        deputado.save!
+        deputados_cache[ide] = deputado.id
       end
 
-      despesa.num_mes = row["numMes"].to_i
-      despesa.num_ano = row["numAno"].to_i
-      despesa.num_sub_cota = row["numSubCota"].to_i
-      despesa.txt_descricao = row["txtDescricao"]
-      despesa.num_especificacao_subcota = row["numExpecificacaoSubcota"].to_i
-      despesa.txt_descricao_especificacao = row["txtDescricaoEspecificacao"]
-      despesa.txt_fornecedor = row["txtFornecedor"]
-      despesa.txt_cnpj_cpf = row["txtCNPJCPF"]
-      despesa.txt_numero = row["txtNumero"]
-      despesa.ind_tipo_documento = row["indTipoDocumento"].to_i
-      despesa.ide_documento = row["ideDocumento"].to_i
-      despesa.vlr_documento = row["vlrdocumento"].to_d
-      despesa.vlr_glosa = row["vlrGlosa"].to_d
+      key = [dep_id, row["ideDocumento"].to_i]
+      desp_id = despesas_cache[key]
 
-      if row["vlrLiquido"].to_d >= 0
-        despesa.vlr_liquido = row["vlrLiquido"].to_d
+      despesa = desp_id ? Despesa.find(desp_id) : Despesa.new(deputado_id: dep_id, ide_documento: key.last)
+
+      desp_attrs = {
+        dat_emissao: Date.parse(row["datEmissao"]),
+        num_mes: row["numMes"].to_i,
+        num_ano: row["numAno"].to_i,
+        num_sub_cota: row["numSubCota"].to_i,
+        txt_descricao: row["txtDescricao"],
+        num_especificacao_subcota: row["numEspecificacaoSubCota"].to_i,
+        txt_descricao_especificacao: row["txtDescricaoEspecificacao"],
+        txt_fornecedor: row["txtFornecedor"],
+        txt_cnpj_cpf: row["txtCNPJCPF"],
+        txt_numero: row["txtNumero"],
+        ind_tipo_documento: row["indTipoDocumento"].to_i,
+        ide_documento: row["ideDocumento"].to_i,
+        vlr_documento: row["vlrDocumento"].to_d,
+        vlr_glosa: row["vlrGlosa"].to_d,
+        vlr_liquido: row["vlrLiquido"].to_d,
+        num_parcela: row["numParcela"].to_i,
+        txt_passageiro: row["txtPassageiro"],
+        txt_trecho: row["txtTrecho"],
+        num_lote: row["numLote"].to_i,
+        num_ressarcimento: row["numRessarcimento"].to_f,
+        dat_pagamento_restituicao: row["datPagamentoRestituicao"].present? ? DateTime.parse(row["datPagamentoRestituicao"]) : nil,
+        vlr_restituicao: row["vlrrestituicao"].to_d,
+        url_documento: row["urlDocumento"]
+      }
+
+      despesa.assign_attributes(desp_attrs)
+
+      if despesa.valid?
+        despesa.save!
+        despesas_cache[key] ||= despesa.id
+        sucesso += 1
       else
-        Rails.logger.warn "Ignorando despesa ideDocumento=#{row['ideDocumento']} por vlrLiquido menor que zero"
-        erro_valor += 1
-        next
+        Rails.logger.warn "Erro na despesa #{key}: #{despesa.errors.full_messages.join(', ')}"
       end
-
-      despesa.num_parcela = row["numParcela"].to_i
-      despesa.txt_passageiro = row["txtPassageiro"]
-      despesa.txt_trecho = row["txtTrecho"]
-      despesa.num_lote = row["numLote"].to_i
-      despesa.num_ressarcimento = row["numRessarcimento"].to_f
-      despesa.dat_pagamento_restituicao = row["datPagamentoRestituicao"].present? ? DateTime.parse(row["datPagamentoRestituicao"]) : nil
-      despesa.vlr_restituicao = row["vlrRestituicao"].to_d
-      despesa.url_documento = row["urlDocumento"]
-
-      despesa.save! if despesa.changed?
-
-      sucesso += 1
     end
-
-    Rails.logger.info "Importação realizada com sucesso para #{@file_path}. #{sucesso} sucesso, #{erro_data} com erro de data e #{erro_valor} com valor líquido menor que zero"
   end
 end
