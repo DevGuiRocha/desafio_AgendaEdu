9c28c602309970de6a6cdec640e43fa757d6decf
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-05-25 23:09:55 -0300

    feat: adicionado validação de unicidade para o campo CPF no model de Deputado

--- ceap-backend/app/models/deputado.rb
+++ ceap-backend/app/models/deputado.rb
@@ -2,7 +2,7 @@ class Deputado < ApplicationRecord
   has_many :despesas, dependent: :destroy
 
   validates :ide_cadastro, presence: true, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true
   validates :nome_parlamentar, presence: true
   validates :sg_uf, presence: true
-  validates :cpf, presence: true
 end
